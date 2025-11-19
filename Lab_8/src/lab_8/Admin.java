78f7fc391e62252ad3c901b8c29c47d76070f104
Donia-Ebeid <ebeiddonia2005@gmail.com>
2025-11-19 16:09:39 +0200

    Create Admin.java

---
+++ Lab_8/src/lab_8/Admin.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab_8;
+
+/**
+ *
+ * @author New Eng
+ */
+public class Admin extends User {
+
+    public Admin(String userId, String username, String email, String passwordHash) {
+        super(userId, Role.ADMIN, username, email, passwordHash);
+    }
+
+     // admin by3ml approve aw reject through el CourseManager
+    public boolean approveCourse(CourseManager courseManager, String courseId) {
+        return courseManager.approveCourse(courseId);
+    }
+
+    public boolean rejectCourse(CourseManager courseManager, String courseId) {
+        return courseManager.rejectCourse(courseId);
+    }
+}
+
