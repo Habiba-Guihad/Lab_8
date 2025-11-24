c67c39e2a22896cab87d36d0c5a63eb02f3cff12
Habiba-Guihad <es.habiba.khalil2023@alexu.edu.eg>
2025-11-24 03:01:19 +0200

    .

--- Lab_8/src/lab_8/StudentDashboardFrame.java
+++ Lab_8/src/lab_8/StudentDashboardFrame.java
@@ -167,11 +167,6 @@ initComponents();
         lblEnrolled.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         lblEnrolled.setText("Enrolled Courses");
 
-        listAvailable.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
         jScrollPane1.setViewportView(listAvailable);
 
         jScrollPane2.setViewportView(listEnrolled);
